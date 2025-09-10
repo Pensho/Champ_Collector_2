40ac1a34425846a5283f50f98436da16ed4707cf
Pensho <Jpensjo@gmail.com>
2025-09-10 21:36:35 +0200

    Battle turn indicator & round buttons
    Added a visual indicator to show which characters turn it is.
    Made the character skill buttons round.

--- Scripts/Worldview/Context_Battle.gd
+++ Scripts/Worldview/Context_Battle.gd
@@ -2,7 +2,9 @@ class_name Context_Battle extends Context
 
 @warning_ignore_start("unused_private_class_variable")
 
-@export var _enemy_characters: Array[CharacterPreset]
+@export var _enemies_wave_1: Array[CharacterPreset]
+@export var _enemies_wave_2: Array[CharacterPreset]
+@export var _enemies_wave_3: Array[CharacterPreset]
 @export var _location: String
 
 @warning_ignore_restore("unused_private_class_variable")
--- Scripts/battle.gd
+++ Scripts/battle.gd
@@ -11,6 +11,8 @@ extends Node2D
 @onready var character_repr_2: CharacterRepresentation = $Character_2
 @onready var character_repr_3: CharacterRepresentation = $Character_3
 
+@onready var turn_indicator: TextureRect = $Turn_Indicator
+
 var _char_1: Character
 var _char_2: Character
 var _char_3: Character
@@ -21,27 +23,30 @@ var _enemy_3: Character
 
 const TURN_POS_X_THRESHOLD: int = 360
 
-var _someones_turn: bool = false
+var _characters_turn: Character = null
 var _initialized: bool = false
 
 func _process(delta: float) -> void:
 	if(_initialized):
-		UpdateBattle(delta, _char_1, 0)
-		UpdateBattle(delta, _char_2, 1)
-		UpdateBattle(delta, _char_3, 2)
-		UpdateBattle(delta, _enemy_1, 3)
-		UpdateBattle(delta, _enemy_2, 4)
-		UpdateBattle(delta, _enemy_3, 5)
+		UpdateBattle(delta, _char_1, 0, character_repr_1)
+		UpdateBattle(delta, _char_2, 1, character_repr_2)
+		UpdateBattle(delta, _char_3, 2, character_repr_3)
+		UpdateBattle(delta, _enemy_1, 3, enemy_repr_1)
+		UpdateBattle(delta, _enemy_2, 4, enemy_repr_2)
+		UpdateBattle(delta, _enemy_3, 5, enemy_repr_3)
 
-func UpdateBattle(delta: float, character: Character, battleSlot: int) -> void:
-	if (_someones_turn):
+func UpdateBattle(delta: float, character: Character, battleSlot: int, representation: CharacterRepresentation) -> void:
+	if (null != _characters_turn):
 		return
 	if(character._currentHealth <= 0):
 		return
 	if(battle_ui._char_turns[battleSlot].position.x + battle_ui._char_turns[battleSlot].get_rect().size.x < TURN_POS_X_THRESHOLD):
 		battle_ui._char_turns[battleSlot].position = battle_ui._char_turns[battleSlot].position + Vector2(character._speed * delta, 0)
 	else:
-		_someones_turn = true
+		_characters_turn = character
+		turn_indicator.position.x = representation.position.x + (representation._character_texture.size.x * 0.5) - (turn_indicator.size.x * 0.5)
+		turn_indicator.position.y = representation.position.y - turn_indicator.size.y
+		turn_indicator.show()
 
 func CharPreperations(p_repr: CharacterRepresentation, p_character: Character) -> void:
 	p_repr._level.text = str(p_character._level)
@@ -67,27 +72,27 @@ func Init(p_context: ContextContainer) -> void:
 	battle_ui._char_turns[1].texture = load(_char_2._texture)
 	battle_ui._char_turns[2].texture = load(_char_3._texture)
 	
-	for enemy in battlecontext._enemy_characters:
+	for enemy in battlecontext._enemies_wave_1:
 		print("found enemy: ", enemy._name)
 	
-	if (battlecontext._enemy_characters.size() >= 1):
+	if (battlecontext._enemies_wave_1.size() >= 1):
 		_enemy_1 = Character.new()
-		_enemy_1.InstantiateNew(battlecontext._enemy_characters[0], -1)
+		_enemy_1.InstantiateNew(battlecontext._enemies_wave_1[0], -1)
 		CharPreperations(enemy_repr_1, _enemy_1)
 		battle_ui._char_turns[3].texture = load(_enemy_1._texture)
 	else:
 		print("Accidental load to battle scene without enemies.")
 		get_tree().quit()
-	if (battlecontext._enemy_characters.size() >= 2):
+	if (battlecontext._enemies_wave_1.size() >= 2):
 		_enemy_2 = Character.new()
-		_enemy_2.InstantiateNew(battlecontext._enemy_characters[1], -1)
+		_enemy_2.InstantiateNew(battlecontext._enemies_wave_1[1], -1)
 		CharPreperations(enemy_repr_2, _enemy_2)
 		battle_ui._char_turns[4].texture = load(_enemy_2._texture)
 	else:
 		enemy_repr_2.hide()
-	if (battlecontext._enemy_characters.size() >= 3):
+	if (battlecontext._enemies_wave_1.size() >= 3):
 		_enemy_3 = Character.new()
-		_enemy_3.InstantiateNew(battlecontext._enemy_characters[2], -1)
+		_enemy_3.InstantiateNew(battlecontext._enemies_wave_1[2], -1)
 		CharPreperations(enemy_repr_3, _enemy_3)
 		battle_ui._char_turns[5].texture = load(_enemy_3._texture)
 	else:
