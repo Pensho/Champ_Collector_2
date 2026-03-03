524841a7ff1c663d59292f8487c0c77fad28d2fb
Pensho <Jpensjo@gmail.com>
2026-03-03 10:23:07 +0100

    Fixed post battle UI dmg bug
    The damage dealt bar now show the correct color depending on a characters damage contribution.

--- Scripts/UI/Post_Battle_UI/character_damage_result.gd
+++ Scripts/UI/Post_Battle_UI/character_damage_result.gd
@@ -6,6 +6,8 @@ extends Control
 @export var _damage_bar: ProgressBar
 @export var _damage_label: Label
 
+var _style_box: StyleBoxFlat = StyleBoxFlat.new()
+
 func SetName(p_name: String) -> void:
 	_name.text = p_name
 
@@ -17,10 +19,7 @@ func SetDamageDealt(p_damage: int, p_total_damage_dealt) -> void:
 	_damage_bar.max_value = p_total_damage_dealt
 	_damage_label.text = str(p_damage)
 	
-	var style_box: StyleBoxFlat = _damage_bar.get_theme_stylebox("fill")
-	
 	var red: float = 1.0 - (float(p_damage) / float(p_total_damage_dealt))
 	var green: float = float(p_damage) / float(p_total_damage_dealt)
-	style_box.bg_color = Color(red, green, 0.0, 1.0)
-	print(style_box.bg_color)
-	_damage_bar.add_theme_stylebox_override("fill", style_box)
+	_style_box.bg_color = Color(red, green, 0.0, 1.0)
+	_damage_bar.add_theme_stylebox_override("fill", _style_box)
