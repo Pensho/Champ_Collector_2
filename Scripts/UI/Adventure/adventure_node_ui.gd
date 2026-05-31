eedf6f21ab0bd75e085a143b1522f098a3b3da08
Pensho <jpensjo@gmail.com>
2026-05-31 16:11:40 +0200

    Add type icons to adventure map nodes

--- Scripts/UI/Adventure/adventure_node_ui.gd
+++ Scripts/UI/Adventure/adventure_node_ui.gd
@@ -2,6 +2,9 @@ class_name AdventureNodeUi extends Control
 
 signal node_selected(p_node: NodeData)
 
+const ICON_FIGHT := preload("res://Assets/Champ_Collector/Creatures/Tidal_Corsair/Tidal_Corsair_Stack_Steel.png")
+const ICON_BOSS := preload("res://Assets/Champ_Collector/Creatures/Tidal_Corsair/Tidal_Corsair_Stack_Sea.png")
+
 @export var _icon: TextureRect
 @export var _button: Button
 
@@ -17,6 +20,11 @@ func Init(p_node: NodeData, p_is_eligible: bool) -> void:
 	else:
 		modulate = Color(0.3, 0.3, 0.3, 0.6)
 	_button.disabled = not p_is_eligible and not p_node.is_complete
+	match p_node.node_type:
+		NodeData.Node_Type.FIGHT:
+			_icon.texture = ICON_FIGHT
+		NodeData.Node_Type.BOSS:
+			_icon.texture = ICON_BOSS
 
 func _on_button_up() -> void:
 	if not _node_data.is_complete:
