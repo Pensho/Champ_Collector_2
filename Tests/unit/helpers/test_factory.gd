67197a4cb9f131b795a2ffc23c38573ed3b5cab1
Pensho <jpensjo@gmail.com>
2026-06-09 17:46:11 +0200

    Add test factory helper
    Shared static builders (make_character, make_loot_table, make_adventure_state)
    for test files that need common setup construction.

---
+++ Tests/unit/helpers/test_factory.gd
@@ -0,0 +1,24 @@
+extends RefCounted
+
+static func make_character() -> Character:
+	var c: Character = load("res://Scenes/Characters/Character.tscn").instantiate()
+	c._name = "TestCharacter"
+	c._level = 1
+	c._experience = 0
+	c._attributes[Types.Attribute.Health] = 10
+	c._attributes[Types.Attribute.Speed] = 5
+	c._attributes[Types.Attribute.Attack] = 8
+	c._attributes[Types.Attribute.Defence] = 6
+	c._attributes[Types.Attribute.Accuracy] = 7
+	c._attributes[Types.Attribute.Resistance] = 6
+	c._attributes[Types.Attribute.Mysticism] = 4
+	c._attributes[Types.Attribute.Knowledge] = 4
+	c._attributes[Types.Attribute.CritChance] = 5
+	c._attributes[Types.Attribute.CritDamage] = 150
+	return c
+
+static func make_loot_table() -> LootTable:
+	return LootTable.new()
+
+static func make_adventure_state() -> AdventureState:
+	return AdventureState.new()
